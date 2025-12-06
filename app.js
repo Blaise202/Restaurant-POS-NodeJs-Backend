1e2f6cb01a569e35b3f94d809eb08fa18a177ac7
Blaise202 <iblaise2023@gmail.com>
2025-12-06 10:44:58 +0000

    create express serve

---
+++ app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express")
+const app = express()
+
+const PORT = process.env.PORT;
+
+app.get("/", (request, response) => {
+  response.json({ message: "POS Server is working!" });
+})
+
+app.listen(PORT, () => {
+  console.log(`POS server is listening on port ${PORT}`);
+})
