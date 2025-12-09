ae257eb7dbc10d06e5817a4a322247cc863cfbb4
Blaise202 <iblaise2023@gmail.com>
2025-12-09 09:22:01 +0000

    login api

--- config/config.js
+++ config/config.js
@@ -3,7 +3,8 @@ require("dotenv").config()
 const config = Object.freeze({ // freeze makes the connection tools read-only
   port: process.env.PORT,
   databaseURI: process.env.MONGODB_URI,
-  nodeEnv: process.env.NODE_ENV
+  nodeEnv: process.env.NODE_ENV,
+  accessTokenSecret: process.env.JWT_SECRET
 })
 
 module.exports = config;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,8 @@
 const createHttpError = require("http-errors");
 const User = require("../models/userModel")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
 
 const users = async (request, response, next) => {
   const users = await User.find()
@@ -44,8 +47,45 @@ const register = async (req, res, next) => {
   }
 }
 
-const login = async (req, response, next) => {
+const login = async (req, res, next) => {
+  try {
 
+    const { email, password } = req.body
+    if (!email || !password) {
+      const error = createHttpError(400, "All fields are required")
+      next(error)
+    }
+
+    const checkUser = await User.findOne({ email });
+    if (!checkUser) {
+      const error = createHttpError(401, "Invalid creadentials")
+      next(error)
+    }
+
+    const checkCreadentials = await bcrypt.compare(password, checkUser.password);
+    if (!checkCreadentials) {
+      const error = createHttpError(401, "Invalid creadentials")
+      next(error)
+    }
+
+    const accessToken = jwt.sign({ _id: checkUser._id }, config.accessTokenSecret, { expiresIn: '1d' })
+
+    res.cookie('accessToken', accessToken, {
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      httpOnly: true,
+      sameSite: "none",
+      secure: true
+    })
+
+    res.status(200).json({
+      success: true,
+      message: "User logged-in Successfully",
+      data: checkUser
+    })
+
+  } catch (error) {
+    next(error)
+  }
 }
 
 module.exports = { users, register, login }
