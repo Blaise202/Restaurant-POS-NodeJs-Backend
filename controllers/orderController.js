a9d32a5709440268bb9c1bf5de58c6f7ae2b218a
Blaise202 <iblaise2023@gmail.com>
2025-12-13 16:16:34 +0000

    order controller

---
+++ controllers/orderController.js
@@ -0,0 +1,19 @@
+
+
+const orders = async () => {
+
+}
+const getOrder = async () => {
+
+}
+const addOrder = async () => {
+
+}
+const updateOrder = async () => {
+
+}
+const deleteOrder = async () => {
+
+}
+
+module.exports = { orders, getOrder, addOrder, updateOrder, deleteOrder }
